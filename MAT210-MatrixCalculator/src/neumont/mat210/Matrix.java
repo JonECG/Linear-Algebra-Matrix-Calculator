e9a8725339dd76e52a75d7529cb5f9bed731f639
Jonathan Pearl <jonathantpearl@gmail.com>
2013-10-24 21:47:37 +0000

    Initial Commit
    git-svn-id: https://svn.neumont.edu:8443/svn/mat210matrixcalculator@1 98595141-ca16-4042-9d9b-b505cfe1f012

---
+++ MAT210-MatrixCalculator/src/neumont/mat210/Matrix.java
@@ -0,0 +1,32 @@
+package neumont.mat210;
+
+public class Matrix 
+{
+	double[][] cells;
+	double determinant;
+	
+	public Matrix(){ this(3); }
+	public Matrix( int dimension ){
+		
+	}
+	
+	private void calculateDeterminant(){
+	}
+	
+	public double getDeterminant()
+	{
+		return determinant;
+	}
+	
+	public void changeDimension( int newDimension ){
+		
+	}
+	
+	public void setCell( int column, int row ){
+		
+	}
+	
+	public double getCell( int column, int row ){
+		return 0;
+	}
+}
---
+++ MAT210-MatrixCalculator/src/neumont/mat210/MatrixMath.java
@@ -0,0 +1,36 @@
+package neumont.mat210;
+
+public class MatrixMath {
+	
+	public static Matrix add( Matrix a, Matrix b ){
+		return null;
+	}
+	
+	public static Matrix subtract( Matrix a, Matrix b ){
+		return null;
+	}
+	
+	public static Matrix multiply( Matrix a, Matrix b ){
+		return null;
+	}
+	
+	public static Matrix scale( double scalar ){
+		return null;
+	}
+	
+	public static Matrix transpose( Matrix mat ){
+		return null;
+	}
+	
+	public static Matrix invert( Matrix mat ){
+		return null;
+	}
+	
+	public static Matrix adjoint( Matrix mat ){
+		return null;
+	}
+	
+	public static Matrix minor( Matrix mat, int column, int row ){
+		return null;
+	}
+}
