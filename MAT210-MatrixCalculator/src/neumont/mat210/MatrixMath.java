623b64abc62b605a62137403d3256c4ac3e0c8a5
landrade <landrade@98595141-ca16-4042-9d9b-b505cfe1f012>
2013-10-25 02:19:21 +0000

    Transpose method completed and matrix adding method started.
    git-svn-id: https://svn.neumont.edu:8443/svn/mat210matrixcalculator@5 98595141-ca16-4042-9d9b-b505cfe1f012

--- MAT210-MatrixCalculator/src/neumont/mat210/MatrixMath.java
+++ MAT210-MatrixCalculator/src/neumont/mat210/MatrixMath.java
@@ -3,38 +3,56 @@ package neumont.mat210;
 public class MatrixMath {
 	
 	public static Matrix add( Matrix a, Matrix b ){
-		return null;
+		Matrix result = new Matrix(a.getDimension());
+		for(int col = 0; col < result.getDimension(); col++)
+		{
+			for(int row = 0; row < result.getDimension(); row++)
+			{
+				
+			}
+		}
+		return result;
 	}
+	
 	
 	public static Matrix subtract( Matrix a, Matrix b ){
 		return null;
 	}
 	
+	
 	public static Matrix multiply( Matrix a, Matrix b ){
 		return null;
 	}
+	
 	
 	public static Matrix scale( double scalar ){
 		return null;
 	}
 	
+
 	public static Matrix transpose( Matrix mat ){
 		Matrix result = new Matrix(mat.getDimension());
-//		for(int row = 0; row < mat.length(); row++)
-//		{
-//			for(int )
-//		}
+		for(int col = 0; col < mat.getDimension(); col++)
+		{
+			for(int row = 0; row < mat.getDimension(); row++)
+			{
+				result.setCell(col, row, mat.getCell(row, col));
+			}
+		}
 		return result;
 	}
+	
 	
 	public static Matrix invert( Matrix mat ){
 		return null;
 	}
 	
+	
 	public static Matrix adjoint( Matrix mat ){
 		return null;
 	}
 	
+	//There is an "array index out-of-bounds error" coming from this method
 	public static Matrix minor( Matrix mat, int column, int row ){
 		Matrix result = new Matrix( mat.getDimension()-1 );
 		for( int i = 0; i < mat.getDimension(); i++ )
