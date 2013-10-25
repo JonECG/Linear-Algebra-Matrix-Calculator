d785095ef14edfdd29003bd21d04b7c9d152df21
Jonathan Pearl <jonathantpearl@gmail.com>
2013-10-25 21:20:18 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/mat210matrixcalculator@18 98595141-ca16-4042-9d9b-b505cfe1f012

---
+++ MAT210-MatrixCalculator/src/neumont/mat210/MatrixPanel.java
@@ -0,0 +1,10 @@
+package neumont.mat210;
+
+import javax.swing.JPanel;
+
+//WindowBuilder - http://download.eclipse.org/windowbuilder/WB/release/R201209281200/4.2/
+//help Install new software
+public class MatrixPanel extends JPanel
+{
+
+}
